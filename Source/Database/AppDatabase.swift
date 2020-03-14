1089e58810bfe29a2d20773143b4da280a02e790
Jason Jobe <box2019@jasonjobe.com>
2020-03-14 17:59:44 -0400

    fixed typos

--- Source/Database/AppDatabase.swift
+++ Source/Database/AppDatabase.swift
@@ -10,37 +10,37 @@ import Foundation
 
 open class AppDatabase: Database {
     
-    public func execute(contentsOfFile file: String) throw {
+    public func execute(contentsOfFile file: String) throws {
         let sql = try String(contentsOfFile: file)
         try executeWrite {
-            try $0.execute(sql)
+            try $0.execute(SQL(sql))
         }
     }
     
-    public func execute(contentsOfURL url: URL) throw {
+    public func execute(contentsOfURL url: URL) throws {
         let sql = try String(contentsOf: url)
         try executeWrite {
-            try $0.execute(sql)
+            try $0.execute(SQL(sql))
         }
     }
 
 // MARK: Application Setting Operations
 
-    public set(_ key: String, to value: Any) {
+    public func set(_ key: String, to value: Any) {
         
     }
     
-    public get(_ key: String) -> Any? {
+    public func get(_ key: String) -> Any? {
         return nil
     }
     
     /// The `append` method  wraps the value into a JSON array
-    public append(_ value: Any, to key: String) {
+    public func append(_ value: Any, to key: String) {
         
     }
     
     /// Removes the value , if found, from the JSON value array
-    public remove(_ value: Any), from key: String) {
+    public func remove(_ value: Any, from key: String) {
     
     }
 }
